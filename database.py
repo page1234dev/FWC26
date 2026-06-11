4d263b005bf70322bc29f9475f72a38e9236d110
page1234dev <page70657@gmail.com>
2026-06-11 03:19:42 +0100

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ import sqlite3
 from datetime import date
 from contextlib import contextmanager
 
-DB_PATH = "fwc26.db"
+import os
+DB_PATH = os.path.join(os.getenv("RAILWAY_VOLUME_MOUNT_PATH", "/data"), "fwc26.db")
 
 @contextmanager
 def get_conn():
